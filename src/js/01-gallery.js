1c7950af685a2316b4695f1a342cd7787ee77a01
Fotyniya <s.kozachenko@ekwo.dp.ua>
2022-12-15 16:52:49 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
+const imgContainer = document.querySelector ('.gallery');
+const imagesMarkup = createImgCardsMarkup(galleryItems);
+
+imgContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+
+const gallery = new SimpleLightbox(imgContainer.querySelectorAll('.gallery a'), {
+    captionsData: 'alt'
+    });
+     
+
+function createImgCardsMarkup (galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+    return `
+    <a class="gallery__item" href="${original}">
+    <img
+      class = "gallery__image"
+      src = "${preview}"
+      data-source ="${original}"
+      alt = "${description}"
+    />
+    </a>
+    `;
+    }).join('')
+}
 console.log(galleryItems);
