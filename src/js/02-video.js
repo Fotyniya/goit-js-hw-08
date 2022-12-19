87277d4065ff3b9fec1f444f419014c98d90ed7b
Fotyniya <s.kozachenko@ekwo.dp.ua>
2022-12-19 20:20:18 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,19 +2,37 @@ import Player from '@vimeo/player';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+let currentTimePlay = {};
 
-// const onPlay = function(iframe) {
-//     console.log (player.duration)  
-// };
-
-// iframe.addEventListener("timeupdate", onPlay);
-
-player.on('play', function() {
-    console.log('played the video!');
-    
-});
+player.on('play', function(){
+    player.setCurrentTime(0).then(function(seconds) {
+        console.log('played the video!');
+        data = localStorage.getItem(LOCALSTORAGE_KEY);
+        if (data) {
+            data = JSON.parse(data);
+            Object.entries(data).forEach(([name, value]) => {
+                currentTimePlay[name] = value;
+                console.log (currentTimePlay)
+                }
+            )
+        }
+    });
+}) 
 
 player.getVideoTitle().then(function(title) {
     console.log('title:', title);
 });
+
+player.on("timeupdate", onPlay);
+
+function onPlay(iframe){
+    console.log('played the video!');
+    currentTimePlay.seconds = iframe.seconds;
+    currentTimePlay.duration = iframe.duration;
+    currentTimePlay.percent = iframe.percent;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentTimePlay));
+    console.log(currentTimePlay);
+};
+
 
